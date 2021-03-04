d33b30759c3cb46c8b4e0adc56092bb78473cb41
eldong <eldon.gormsen@weighdown.com>
2021-03-03 19:25:17 -0600

    Update QuotesController.cs

--- mywebapi/Controllers/QuotesController.cs
+++ mywebapi/Controllers/QuotesController.cs
@@ -22,8 +22,9 @@ namespace mywebapi.Controllers
             List<string> Quotes = new List<string>();
 
             // [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Demo")]
-            connString = "Server=tcp:contosobikedb.database.windows.net,1433;" + "Initial Catalog=contosodb;Persist Security Info=False;" + "User ID=dbadmin;Password=DBpass!2;MultipleActiveResultSets=False;" +
-                "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //connString = "Server=tcp:contosobikedb.database.windows.net,1433;" + "Initial Catalog=contosodb;Persist Security Info=False;" + "User ID=dbadmin;Password=DBpass!2;MultipleActiveResultSets=False;" +
+            //    "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            connString = _configuration.GetConnectionString("myDbConnString");                
 
             string queryString = "SELECT * from Quotes";
 
